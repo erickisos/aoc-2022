70a8731f9432a21541327b4630c0b4dd4e6c142c
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-12-22 23:42:32 -0600

    Add part-02 (#7)

--- src/aoc/days/03.clj
+++ src/aoc/days/03.clj
@@ -7,22 +7,29 @@
   (let [threshold (if (Character/isUpperCase item) 38 96)]
     (- (int item) threshold)))
 
-(defn- calc-priorities
-  [rs]
-  (let [perfect-half (/ (count rs) 2)]
-    (->> (split-at perfect-half rs)
-         (map set)
-         (apply set/intersection)
-         (map item->priority))))
+(defn calc-priorities
+  [items]
+  (->> items
+       (map set)
+       (apply set/intersection)
+       (map item->priority)))
+
+(defn sum-priorities
+  [items]
+  (reduce + (flatten (map calc-priorities items))))
 
 (defn part-01
   [rucksacks]
-  (->> rucksacks
-       (map calc-priorities)
-       flatten
-       (reduce +)))
+  (let [divide #(split-at (/ (count %) 2) %)]
+    (map divide rucksacks)))
+
+(defn part-02
+  [rucksacks]
+  (partition 3 rucksacks))
 
 (defn main
   [filename]
   (let [rucksacks (load! filename)]
-    {:part-01 (part-01 rucksacks)}))
+    {:part-01 (sum-priorities (part-01 rucksacks))
+     :part-02 (sum-priorities (part-02 rucksacks))}))
+
